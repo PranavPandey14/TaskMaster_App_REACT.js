e0e6dbf6a33b094932cccfd25c729f7583111fd8
PRANAV PANDEY <pranavpandey12245@gmail.com>
2023-07-26 12:15:52 +0530

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,12 @@ export default function Navbar() {
       {() => (
         <>
           <div className="mx-auto  lg:px-8 text-red-600  text-3xl p-3">
-            <h1>TaskMaster</h1>
+            <h1>
+              TaskMaster :{" "}
+              <span className="text-white text-lg">
+                Your Ultimate Todo List App
+              </span>{" "}
+            </h1>
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden"></div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
